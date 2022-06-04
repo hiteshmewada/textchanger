2c616880b05b2bcaa095b81051123b7396cb905e
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-04 16:44:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import TextForm from './components/TextForm';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import TextForm from "./components/TextForm";
+// import About from "./components/About";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+// Can't resolve 'react-router-dom' in 'C:\Users\Archer\Desktop\react_course\textchanger\src' This error was resolved using ->  npm install react-router-dom --save
+// if you want to use Switch then install react-router-dom version 5. 
+// Switch is replaced in react-router-dom version 6.
+// npm install react-router-dom@5
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => [
+    setAlert({
+      msg: message,
+      type: type,
+    }),
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500),
+  ];
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "dark";
+      showAlert("Light Mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "white";
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+      // setInterval(() => {
+      //   document.title="Virus In";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title="Virus Out ";
+      // }, 1500);
+    }
+  };
   return (
-    <div className="App">
-      <Header title="TextUtils" about="About"/>
-      {/* <Header/> */}
-      <div className="container my-3">
-
-      <TextForm heading="Enter the text to analyze below" />
-      </div>
-    </div>
+    <>
+      {/* <Router> */}
+        <div
+          className="App"
+          style={{ backgroundColor: mode === "dark" ? "#042743" : "white" }}
+        >
+          <Header
+            title="TextUtils"
+            about="About"
+            mode={mode}
+            toggleMode={toggleMode}
+          />
+          {/* <Header/> */}
+                <Alert alert={alert} />
+          <div className="container my-3">
+            {/* <Switch>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/"> */}
+              <TextForm
+              heading="Enter the text to analyze below"
+              mode={mode}
+              showAlert={showAlert}
+            />
+              {/* </Route>
+            </Switch> */}
+            
+            {/* <About/> */}
+          </div>
+        </div>
+      {/* </Router> */}
+    </>
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,30 +5,32 @@ export default function TextForm(props) {
         // console.log("Uppercase was clicked"+text);
         let newText=text.toUpperCase();
         setText(newText);
-        
+        props.showAlert("Text became uppercased","success");
     }
     const handleLoClick=()=>{
         // console.log("Uppercase was clicked"+text);
         let newText=text.toLowerCase();
         setText(newText);
-        
+        props.showAlert("Text became lowercased","success");
     }
     const handleClearClick=()=>{
         // console.log("Uppercase was clicked"+text);
         let newText="";
         setText(newText);
-        
+        props.showAlert("Text cleared","success");
     }
     const speak=()=>{
         let msg=new SpeechSynthesisUtterance();
         msg.text=text;
         window.speechSynthesis.speak(msg);
+        props.showAlert("Now, you can hear the text","success");
     }
     const removeSymbols=()=>{
         const regex=/[0-9/A-Z/a-z/ /]/g;
         const letters=text.match(regex);
         const res1=letters.join('');
         setText(res1);
+        props.showAlert("All symbols removed","success");
     }
     const wordCount=(text)=>{
         // let wodCount = text.match(/(\w+)/g).length;
@@ -37,6 +39,7 @@ export default function TextForm(props) {
         text = text.replace(/(^\s*)|(\s*$)/gi,""); //exclude  start and end white-space
         text = text.replace(/[ ]{2,}/gi," ");//2 or more space to 1
         text = text.replace(/\n /,"\n"); // exclude newline with a start spacing
+        
         return text.split(' ').filter(function(text){return text!="";}).length;
         //return s.split(' ').filter(String).length; - this can also be used
     }
@@ -50,6 +53,7 @@ export default function TextForm(props) {
         const dig=text.match(regex);
         const res=dig.join('');
         setText(res);
+        props.showAlert("Numbers extracted","success");
     }
     const revText=()=>{
         // convert string to array
@@ -59,31 +63,48 @@ export default function TextForm(props) {
         // cnvert array to string
         let newText=strArr.join("");
         setText(newText);
+        props.showAlert("Given text reversed","success");
+    }
+    const copyText=()=>{
+        var text=document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to your clipboard","success");
+    }
+    const removeExtraSpaces=()=>{
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed","success");
     }
     const [text,setText]=useState("");
     return (
         <>
-        <div className='container'>
+        {/* If you want to write js than use one {} and {{}} for js object */}
+        <div className='container' style={{color:props.mode==="dark"?"white":"black"}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <label htmlFor="myBox" className="form-label">Enter your text below</label>
-                <textarea className="form-control" id="myBox" rows="10" value={text} onChange={handleOnChange} placeholder={"Enter your text here"}></textarea>
+                <textarea className="form-control" id="myBox" rows="10" value={text} onChange={handleOnChange} placeholder={"Enter your text here"} style={{backgroundColor:props.mode==="dark"?"#042743":"white",color:props.mode==="dark"?"white":"black"}}></textarea>
             </div>
-                <button className="btn btn-primary my-3" onClick={handleUpClick}>Covert to Uppercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Covert to Lowercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Covert to Clear</button>
+                <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
                 <button className="btn btn-warning mx-2" onClick={speak}>Speak</button>
                 <br />
                 <button className="btn btn-primary mx-3" onClick={removeSymbols}>Remove all symbols</button>
                 <button className="btn btn-primary mx-3" onClick={extractNum}>Extract Numbers</button>
                 <button className="btn btn-primary my-3" onClick={revText}>Reverse Text</button>
+                <button className="btn btn-primary mx-3" onClick={copyText}>Copy Text</button>
+                <br />
+                <button className="btn btn-primary mx-3" onClick={removeExtraSpaces}>Remove Extra Spaces</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color:props.mode==="dark"?"white":"black"}}>
             <h2>Your text Summary:</h2>
             <p> {wordCount(text)} words and {text.length} characters</p>
             <p>{0.008*text.length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p style={{color:text.length===0?"grey":props.mode==="dark"?"white":"black"}}>{text.length>0?text:"Enter some text in the above textbox to see preview"}</p>
+            
         </div>
         </>
     )
